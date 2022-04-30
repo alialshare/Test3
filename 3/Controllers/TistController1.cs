6469255075031f4686e3934e3bbc32fac78e4b14
ЕлшерифАлиСаадАлиБай <@DESKTOP-BPUT177>
2022-04-30 05:29:58 +0500

    Add project files.

---
+++ 3/Controllers/TistController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3.Controllers
+{
+    public class TistController1 : Controller
+    {
+      public string Hello()
+      {
+            return "hello first control";
+      } 
+        public IActionResult FirstPage()
+        {
+
+            return View();
+        }
+    }
+}
